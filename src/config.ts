dc43f59d1fbe4b654617fa6502597027ce734cc7
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-12-19 17:29:15 +0000

    Some more changes

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,57 @@
-export type Sidebar = Record<string, { link: string }>
-
-export const GUIDES_SIDEBAR: Sidebar = {
-  'Getting Started': {
-    link: '/getting-started',
-  },
+export type NavItem = {
+  title?: string
+  link: string
+  // This is a bootstrap icon
+  icon?: string
+  children?: NavItem[]
 }
+
+export const LEADING_NAVIGATION: NavItem[] = [
+  {
+    title: 'Guides',
+    link: '/',
+    children: [
+      { title: 'Getting Started', link: '/' },
+      { title: 'Development', link: '/' },
+      { title: 'Debugging', link: '/' },
+      { title: 'Testing', link: '/' },
+      { title: 'Building', link: '/' },
+      { title: 'Distribution', link: '/' },
+      { title: 'Features', link: '/' },
+    ],
+  },
+  {
+    title: 'References',
+    link: '/',
+    children: [
+      { title: 'Tauri Architecture', link: '/' },
+      { title: 'Configuration File', link: '/' },
+      { title: 'Tauri CLI', link: '/' },
+      { title: 'JavaScript API', link: '/' },
+      { title: 'Rust API', link: '/' },
+    ],
+  },
+  {
+    title: 'About',
+    link: '/',
+  },
+]
+
+export const SOCIALS: NavItem[] = [
+  {
+    link: '/',
+    icon: 'bi-github',
+  },
+  {
+    link: '/',
+    icon: 'bi-discord',
+  },
+  {
+    link: '/',
+    icon: 'bi-twitter',
+  },
+  {
+    link: '/',
+    icon: 'bi-mastodon',
+  },
+]
